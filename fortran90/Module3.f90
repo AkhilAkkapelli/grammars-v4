c60b83eb14849f9df403cccddfb542c1d19c71bb
Akhil Akkapelli <41839847+AkhilAkkapelli@users.noreply.github.com>
2023-06-04 17:46:34 +0530

    Create Module3.f90

---
+++ fortran90/Module3.f90
@@ -0,0 +1,39 @@
+MODULE Module3
+USE Module5, ONLY : Module5Subroutine2
+IMPLICIT NONE
+
+PRIVATE :: Module3Subroutine4, Module3Subroutine5
+
+CONTAINS
+ 
+SUBROUTINE Module3Subroutine1
+
+!SUBROUTINE BODY
+
+END SUBROUTINE Module3Subroutine1
+
+SUBROUTINE Module3Subroutine2
+
+!SUBROUTINE BODY
+
+END SUBROUTINE Module3Subroutine2
+
+SUBROUTINE Module3Subroutine3
+
+!SUBROUTINE BODY
+
+END SUBROUTINE Module3Subroutine3
+
+SUBROUTINE Module3Subroutine4
+
+!SUBROUTINE BODY
+
+END SUBROUTINE Module3Subroutine4
+
+SUBROUTINE Module3Subroutine5
+
+!SUBROUTINE BODY
+
+END SUBROUTINE Module3Subroutine5
+
+END MODULE Module3
